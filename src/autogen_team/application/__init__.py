9942b1ba95da6105d5328dc185922a8c82e2325a
lgcorzo <lgcorzo@gmail.com>
2026-01-15 10:25:49 +0100

    refactor(application): Move jobs to application layer with relative imports
    - Create application/jobs/ with DDD structure
    - Use relative imports to avoid circular dependencies
    - Export JobKind type and all job classes

--- src/autogen_team/application/__init__.py
+++ src/autogen_team/application/__init__.py
@@ -1,26 +1,5 @@
-"""High-level jobs of the project."""
+"""Application Layer."""
 
-# %% IMPORTS
+from . import jobs
 
-from autogen_team.jobs.evaluations import EvaluationsJob
-from autogen_team.jobs.explanations import ExplanationsJob
-from autogen_team.jobs.inference import InferenceJob
-from autogen_team.jobs.promotion import PromotionJob
-from autogen_team.jobs.training import TrainingJob
-from autogen_team.jobs.tuning import TuningJob
-
-# %% TYPES
-
-JobKind = TuningJob | TrainingJob | PromotionJob | InferenceJob | EvaluationsJob | ExplanationsJob
-
-# %% EXPORTS
-
-__all__ = [
-    "TuningJob",
-    "TrainingJob",
-    "PromotionJob",
-    "InferenceJob",
-    "EvaluationsJob",
-    "ExplanationsJob",
-    "JobKind",
-]
+__all__ = ["jobs"]
